c13ff9c759cf1ff981ca367bd3037c660954987c
RoLo <nicolas.buitrago.roldan@gmail.com>
2016-04-01 12:28:47 -0500

    lo mejore

--- consultarCama.php
+++ consultarCama.php
@@ -1,8 +1,8 @@
 <?php
 include('functions.php'); 
-$cama=$_GET['camarote'];
+$cama=$_GET['cama'];
 if ($resultset = getSQLResultSet("SELECT cedula,nombre,edad,sexo,telefono,direccion,responsable,religion,piso,cama,fecha_ingreso,ant_familiares,ant_patologicos,ant_no_patologicos,ant_gineco_obstetricos,habitacion 
-FROM  `tablapis` where camarote='$camarote'")) {
+FROM  `tablapis` where cama='$cama'")) {
 	
     	while ($row = $resultset->fetch_array(MYSQLI_NUM)) {
     	echo json_encode($row);
